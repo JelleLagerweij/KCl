5213c542f79430789b0da43d03a5bd8b754c9039
JelleLagerweij <v.j.lagerweij@student.tudelft.nl>
2023-03-09 20:02:02 +0100

    hm small errror

--- RUN.sh
+++ RUN.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-runfile=$(expr runMD_H)	# Server where to run
+runfile=$(expr runMD_D)	# Server where to run
 dt=$(expr 2)            # Timestep in fs
-Nruneq=$(expr 100)	# initiation timestep -2
-Nrun1=$(expr 5000)	# dt*Nrun = 0.25 ns of data per run -3
-Nrun2=$(expr 2000)	# dt*Nrun = 1ns of data per run -3
-Nrun3=$(expr 5000)	# dt*Nrun = 2.5ns of data per run -4
+Nruneq=$(expr 100)	# initiation timestep 
+Nrun1=$(expr 5000)	# dt*Nrun = 10ns of data per run
+Nrun2=$(expr 2000)	# dt*Nrun = 4ns of data per run
+Nrun3=$(expr 5000)	# dt*Nrun = 100ns of data per run
 Temp=$(expr 298.15)		# Temperature in K
 Press=$(expr 5)			# Pressure in atm
 
@@ -17,12 +17,12 @@ do
 	mkdir $folder
 	cd $folder
 
-	for m in 4 #3 2 1
+	for m in 4 3 2 1
 	do
 		mkdir m_$m
 		cd m_$m
 
-		for i in 1 #2 3 4
+		for i in 1 2 3 4 5
 		do
 			mkdir $i
 			cd $i
@@ -44,7 +44,6 @@ do
 			sed -i 's/Nrun1_VALUE/'$Nrun1'/' simulation.in
 			sed -i 's/Nrun2_VALUE/'$Nrun2'/' simulation.in
 			sed -i 's/Nrun3_VALUE/'$Nrun3'/' simulation.in
-			sed -i 's/Nrun4_VALUE/'$Nrun4'/' simulation.in
 
 			# Set filder location
 			sed -i 's/run_FOLDER/'$i'/' copy_files.sh
